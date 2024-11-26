84d200d30dc99e2ba53a190a1d5c2c1d20717155
putubagus13 <putu031216@gmail.com>
2024-11-27 02:20:39 +0700

    fix: header navbar media query mobile

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -12,10 +12,13 @@ export default function LayoutContent({
 }>) {
   return (
     <div className="relative w-full h-full flex justify-between flex-col overflow-scroll">
-      <header className="h-20 py-1 lg:px-[5%] lg:py-10 items-center fixed w-full top-0 z-50 bg-foreground/20">
-        <div className="relative h-14 lg:h-24 flex items-center w-full rounded-badge lg:rounded-full bg-white/10 backdrop-blur-md border-white border-[1px] border-opacity-30">
+      <header className="h-20 lg:px-[5%] lg:py-10 items-center fixed w-full top-0 z-50 bg-foreground/20">
+        <div className="relative h-14 lg:h-24 flex items-center w-full lg:rounded-full bg-white/10 backdrop-blur-md lg:border-white lg:border-[1px] border-opacity-30">
           <nav className="flex flex-row lg:flex-row w-full justify-between px-[5%]">
-            <div className="h-12 w-28 lg:w-32 lg:h-28 overflow-hidden">
+            <Link
+              href={HOME}
+              className="h-12 w-28 lg:w-32 lg:h-28 overflow-hidden cursor-pointer"
+            >
               <Image
                 width={500}
                 height={500}
@@ -23,7 +26,7 @@ export default function LayoutContent({
                 className="w-full h-full object-cover"
                 alt="captivad"
               />
-            </div>
+            </Link>
             <div className="hidden lg:flex gap-6 text-foreground items-center">
               <Navbar mediaQuery="desktop" />
             </div>
@@ -37,22 +40,30 @@ export default function LayoutContent({
       <main>{children}</main>
       <footer className=" h-full w-full bg-foreground px-[5%] py-10 md:px-20">
         <Image src={CaptivadLogoFooter} alt="logo" width={240} height={137} />
-        <div className="flex flex-wrap justify-between items-center gap-4">
-          <div className="text-primary">
+        <div className="grid grid-cols-1 lg:grid-cols-3 justify-between items-center gap-4">
+          <div className="text-primary col-span-1 lg:col-span-2">
             <h6 className=" font-bold">CaptivAd Office</h6>
             <p className="pt-4">
               AD Premier Office Park 9th Floor, Jl. TB Simatupang No.5 Jakarta
               Selatan 12550
             </p>
           </div>
-          <div className="flex gap-4 flex-wrap justify-around lg:w-1/2 items-center">
-            <div className="text-primary font-semibold flex flex-col gap-2">
-              <Link href={HOME}>Home</Link>
-              <Link href={OUR_SERVICES}>Our Services</Link>
+          <div className="gap-4 grid grid-cols-2 justify-around items-center col-span-1">
+            <div className="col-span-1 text-primary font-semibold flex flex-col gap-2">
+              <Link href={HOME}>
+                <h5>Home</h5>
+              </Link>
+              <Link href={OUR_SERVICES}>
+                <h5>Our Services</h5>
+              </Link>
             </div>
-            <div className="text-primary font-semibold flex flex-col gap-2">
-              <Link href={""}>Our Work</Link>
-              <Link href={CONTACT_US}>Contact As</Link>
+            <div className="col-span-1 text-primary font-semibold flex flex-col gap-2">
+              <Link href={""}>
+                <h5>Our Work</h5>
+              </Link>
+              <Link href={CONTACT_US}>
+                <h5>Contact Us</h5>
+              </Link>
             </div>
           </div>
         </div>
